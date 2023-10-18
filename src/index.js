d3f553fb66cf2461ccd6af71c037081469d64048
Gabriel Guimaraes <gabriel.mg04@outlook.com>
2023-10-18 20:29:35 +0100

    Finished, can insert the number tab into the page.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import './style.css';
 import insert from './insert-content.js';
+import { forEach } from 'lodash';
 
 function component() {
+    const content = document.querySelector('#content');
+    const btns = document.querySelectorAll('li');
+    btns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            content.innerHTML = insert(btn.innerHTML)
+        });
+});
 }
 
 component();
--- src/insert-content.js
+++ src/insert-content.js
@@ -1,7 +1,3 @@
-const insertDiv = document.createElement('div');
-
 export default function insert(btnContent) {
-    insertDiv.textContent = `This is tab ${btnContent[btnContent.length]}`;
-
-    return {insertDiv};
+    return `This is tab ${btnContent[btnContent.length - 1]}`;
 }
